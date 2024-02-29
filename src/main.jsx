799ac580ada36044b8ceaf75126b3e1cd6b43c49
MaJoReGo <126532332+MaJoReGo@users.noreply.github.com>
2024-02-29 14:51:21 -0500

    App

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
